b3622a45ac30402236ca1f85e0ce56ee94718bed
kuralbek <kuralbek41@gmail.com>
2018-12-04 21:52:22 +0500

    EcoMarket version 1

---
+++ js/script.js
@@ -0,0 +1,65 @@
+let btn_menu = document.getElementById('btn-menu'),
+    headerNav = document.querySelector('.header-nav');
+    
+    btn_menu.addEventListener ('click', function(){
+        if(headerNav.style.display == 'block'){
+            headerNav.style.display = 'none';
+            
+            btn_menu.innerHTML='&#9776';
+        }else{
+            headerNav.style.display = 'block';
+            btn_menu.innerHTML='&#9747;';
+            
+            
+        }
+    })
+
+    $(document).ready(function(){
+        $('.slider-one').owlCarousel({
+            rtl:false,
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:5000,
+            margin:10,
+            nav:false,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:1
+                }
+            }
+      });
+
+     
+    
+
+    // Second Slider
+
+    $(document).ready(function(){
+        $(".slide-two").owlCarousel();
+      });
+    $(".slide-two").owlCarousel({
+		loop:true, //Зацикливаем слайдер
+		margin:10, //Отступ от картино если выводите больше 1
+		nav:false, //Отключил навигацию
+		autoplay:false, //Автозапуск слайдера
+		smartSpeed:2000, //Время движения слайда
+		autoplayTimeout:4000, //Время смены слайда
+		responsive:{ //Адаптация в зависимости от разрешения экрана
+			0:{
+				items:1
+			},
+			600:{
+				items:2
+			},
+			1000:{
+				items:3
+			}
+		}
+	});
+});
